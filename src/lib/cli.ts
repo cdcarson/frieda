33057075712dd7fc24244d5e4d45ebc415d1b347
Christopher Carson <chris@nowzoo.com>
2023-04-13 11:20:15 -0500

    simplify

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-import { main } from './cli/index.js';
 import { config} from 'dotenv'
 
 config();
-try {
-  await main(process.cwd(), process.argv.slice(2), process.env)
-} catch (error) {
-  console.log(error);
-  process.exit(1);
-}
+console.log('This works!')
+
 
 export default {};
 
