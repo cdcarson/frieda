e30ce54d7b1f2594800bec1f4405430b19b397cb
Christopher Carson <chris@nowzoo.com>
2023-04-27 17:29:40 -0500

    commands

--- src/lib/cli/cmd-migrate.ts
+++ src/lib/cli/cmd-migrate.ts
@@ -4,16 +4,41 @@ import {
   cliGetSettings,
   promptModel,
   cancelAndExit,
-  cliPromptRunMigration
+  cliPromptRunMigration,
+  cliPromptEditMigration
 } from './cli.js';
 
 import { isCancel, select, text, confirm, log } from '@clack/prompts';
 import { DEFAULT_JSON_FIELD_TYPE } from './constants.js';
 import { KNOWN_MYSQL_TYPES } from '$lib/api/constants.js';
+import type { FileResult, FileSystemResult } from './types.js';
+import { getFileResult } from './file-system.js';
+import { fmtPath } from './utils.js';
 
 export const cmdMigrate = async (rawArgs: string[]) => {
   const settings = await cliGetSettings();
-  const { schema, models } = await cliFetchSchema(settings);
+  const { schema } = await cliFetchSchema(settings);
   const file = rawArgs[0];
-  console.log(file)
+  let fileResult: FileResult|null;
+
+  if (file) {
+    fileResult = await getFileResult(file);
+    if (! fileResult.exists) {
+      log.warn(`File ${fmtPath(fileResult.relativePath)} does not exist.`)
+    } else if (! fileResult.isFile) {
+      log.warn(`File ${fmtPath(fileResult.relativePath)} is not a file.`)
+    } else {
+      return await cliPromptRunMigration(settings, {
+        sql: fileResult.contents || '',
+        isCurrentMigrationSql: false,
+        schemaBefore: schema
+      })
+    }
+    
+  }
+  return await cliPromptEditMigration(settings, {
+    sql: '',
+    isCurrentMigrationSql: false,
+    schemaBefore: schema
+  })
 };
