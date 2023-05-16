fb5ac28283799ed6afe2cb53d31bc0fc23495874
Christopher Carson <chris@nowzoo.com>
2023-05-16 07:58:32 -0500

    automate

--- scripts/write-version.js
+++ scripts/write-version.js
@@ -1,7 +1,8 @@
-import pkg from '../package.json' assert { type: 'json' };
+#! /usr/bin/env node
+
 import fs from 'fs-extra';
 import { join } from 'node:path';
 fs.writeFileSync(
   join(process.cwd(), 'src', 'lib', 'version.ts'),
-  `export const FRIEDA_VERSION = '${pkg.version}';`
+  `export const FRIEDA_VERSION = '${process.env.npm_package_version}';`
 );
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,1 +1,1 @@
-export const FRIEDA_VERSION = '0.0.32';
+export const FRIEDA_VERSION = '0.0.33';
